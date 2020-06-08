0c02a1d4892a4afb3564e6380bd71a9d7b41c67c
Govert de Gans <grappigegovert@hotmail.com>
2020-06-08 22:28:15 +0200

    runbot.bat: clear ytdl cache on start + remove weird reg edit setup

--- runbot.bat
+++ runbot.bat
@@ -3,22 +3,7 @@
 CHCP 65001 > NUL
 CD /d "%~dp0"
 
-SETLOCAL ENABLEEXTENSIONS
-SET KEY_NAME="HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-SET VALUE_NAME=HideFileExt
-
-FOR /F "usebackq tokens=1-3" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
-    SET ValueName=%%A
-    SET ValueType=%%B
-    SET ValueValue=%%C
-)
-
-IF x%ValueValue:0x0=%==x%ValueValue% (
-    ECHO Unhiding file extensions...
-    START CMD /c REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
-)
-ENDLOCAL
-
+youtube-dl --rm-cache-dir
 
 IF EXIST %SYSTEMROOT%\py.exe (
     CMD /c %SYSTEMROOT%\py.exe -3.5 run.py
@@ -35,3 +20,4 @@ GOTO end
 ECHO ERROR: Python has either not been installed or not added to your PATH.
 
 :end
+pause
